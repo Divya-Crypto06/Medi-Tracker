05b3b5f1b2cb084c6848eae7814438f199fb84fa
Divya-Crypto06 <katta.chowdary13@gmail.com>
2025-02-02 23:54:10 -0500

    Update signUp.jsx

--- app/login/signUp.jsx
+++ app/login/signUp.jsx
@@ -1,10 +1,80 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput,StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Colors from '../../constant/Colors'
+import { useRouter } from 'expo-router'
 
 export default SignUp = () => {
+  const router=useRouter();
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+        <View style={{
+            padding:25
+        }}>
+          <Text style={styles.textHeader}>Create New Account</Text>
+        <View style={{
+          marginTop:25
+        }}>
+          <Text>Email</Text>
+          <TextInput placeholder='Email' style={styles.textInput}/>
+        </View>
+        <View style={{
+          marginTop:25
+        }}>
+          <Text>Password</Text>
+          <TextInput placeholder='Password'
+          secureTextEntry={true} style={styles.textInput}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{
+            fontsize:17,
+            color:'white',
+            textAlign:'center'
+          }}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonCreate}
+        onPress={()=>router.push('login/signUp')}>
+          <Text style={{
+            fontsize:17,
+            color:Colors.PRIMARY,
+            textAlign:'center'
+          }}>Create Account</Text>
+        </TouchableOpacity>
+        </View>
   )
 }
+
+const styles=StyleSheet.create({
+    textHeader:{
+        fontsize:30,
+        fontWeight:'bold',
+        marginTop:15
+    },
+    subText:{
+        fontsize:30,
+        fontWeight:'bold',
+        marginTop:10,
+        color:Colors.GRAY
+    },
+    textInput:{
+      padding:10,
+      borderWidth:1,
+      fontSize:17,
+      borderRadius:10,
+      marginTop:5,
+      backgroundColor:'white'
+    },
+    button:{
+      padding:20,
+      backgroundColor:Colors.PRIMARY,
+      borderRadius:10,
+      marginTop:35
+    },
+    buttonCreate:{
+      padding:20,
+      backgroundColor:'white',
+      borderRadius:10,
+      marginTop:20,
+      borderWidth:1,
+      borderColor:Colors.PRIMARY
+    }
+})
