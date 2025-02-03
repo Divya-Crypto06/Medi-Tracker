8dd24305e8d6d74dc320b655d2fcf197c5eaeb8b
Divya Crypto <katta.chowdary13@gmail.com>
2025-02-02 21:45:35 -0500

    Made code changes

---
+++ app/(tabs)/AddNew.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Addnew() {
+  return (
+    <View>
+      <Text>Addnew</Text>
+    </View>
+  )
+}
--- app/login/signIn.jsx
+++ app/login/signIn.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Colors from '../../constant/Colors'
 
@@ -10,6 +10,24 @@ export default function signIn(){
       <Text style={styles.textHeader}>Let's Sign You In</Text>
       <Text style={styles.subText}>Welcome Back</Text>
       <Text style={styles.subText}>You've been missed</Text>
+
+    <View style={{
+      marginTop:25
+    }}>
+      <Text>Email</Text>
+      <TextInput placeholder='Email' style={styles.textInput}/>
+    </View>
+    <View style={{
+      marginTop:25
+    }}>
+      <Text>Password</Text>
+      <TextInput placeholder='Password'
+      secureTextEntry={true} style={styles.textInput}
+      />
+    </View>
+    <TouchableOpacity style={styles.button}>
+      <Text>Login</Text>
+    </TouchableOpacity>
     </View>
   )
 }
@@ -25,5 +43,16 @@ const styles=StyleSheet.create({
         fontWeight:'bold',
         marginTop:10,
         color:Colors.GRAY
+    },
+    textInput:{
+      padding:10,
+      borderWidth:1,
+      fontSize:17,
+      borderRadius:10,
+      marginTop:5,
+      backgroundColor:'white'
+    },
+    button:{
+      padding:20
     }
 })
