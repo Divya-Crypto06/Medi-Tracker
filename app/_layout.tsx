9f56b23e004a9db6ba802a921017efbb6d576e4d
Divya-Crypto06 <katta.chowdary13@gmail.com>
2025-02-04 15:36:19 -0500

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,53 @@
-import { Stack } from "expo-router";
 
+import React from 'react'
+import { Tabs, useRouter } from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../../config/FirebaseConfig';
 
-export default function RootLayout() {
+export default function TabLayout() {
+
+  const router=useRouter();
+  //if user login or not
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+      console.log(uid)
+      // ...
+    } else {
+      router?.push('/login')
+      // User is signed out
+      // ...
+    }
+  });
   return (
-   <Stack screenOptions={{
-    headerShown:false
-   }
-   }>
-    <Stack.Screen name="(tabs)" />
-    <Stack.Screen name="login" />
-    </Stack>
+    <Tabs screenOptions={{
+      headerShown:false
+    }}>
+        <Tabs.Screen name='index'
+        options={{
+          tabBarLabel:'Home',
+          tabBarIcon:({color,size}) =>(
+          <FontAwesome name="home" size={size} color="color" />
+        )
+      }} />
+        <Tabs.Screen name='AddNew'
+        options={{
+          tabBarLabel:'Add New',
+          tabBarIcon:({color,size}) =>(
+          <FontAwesome name="plus-square" size={size} color="color" />
+        )
+      }} />
+        <Tabs.Screen name='Profile'
+        options={{
+          tabBarLabel:'Profile',
+          tabBarIcon:({color,size}) =>(
+          <FontAwesome name="user" size={size} color="color" />
+        )
+      }} />
+    </Tabs>
   )
-  }
+}
+
