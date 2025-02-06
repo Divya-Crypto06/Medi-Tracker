4c4aec5a49bf8d4ab7dec386c3cd90d93beede95
Divya-Crypto06 <katta.chowdary13@gmail.com>
2025-02-05 23:10:22 -0500

    Create Options.jsx

---
+++ constant/Options.jsx
@@ -0,0 +1,27 @@
+export const TypeList=[
+    {
+        name:'Tablet',
+        icon:'https://www.flaticon.com/free-icon/drugs_4320365?term=tablets+pharma&page=1&position=3&origin=search&related_id=4320365'
+    },
+    {
+        name:'Capsules',
+        icon:'https://www.flaticon.com/free-icon/capsule_8870385?term=capsules&page=1&position=7&origin=search&related_id=8870385'
+    },
+    {
+        name:'Drops',
+        icon:'https://www.flaticon.com/free-icon/drop-of-liquid_31823?term=drops+medicine&page=1&position=1&origin=search&related_id=31823'
+    },
+    {
+        name:'Syrup',
+        icon:'https://www.flaticon.com/free-icon/syrup_3209036?term=syrups&page=1&position=2&origin=search&related_id=3209036'
+    },
+    {
+        name:'Injection',
+        icon:'https://www.flaticon.com/free-icon/drugs_3217557?term=injection+medicine&page=1&position=4&origin=search&related_id=3217557'
+    },
+    {
+        name:'Other',
+        icon:'https://www.flaticon.com/free-icon/option_3018442?term=other+option&page=1&position=4&origin=search&related_id=3018442'
+
+    }
+]
