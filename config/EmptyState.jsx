06902ebb6ee0869c828559fc14484e18498a3b41
Divya-Crypto06 <katta.chowdary13@gmail.com>
2025-02-09 21:00:51 -0500

    Create EmptyState.jsx

---
+++ config/EmptyState.jsx
@@ -0,0 +1,47 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import ConstantString from '../constant/ConstantString'
+import { useRouter } from 'expo-router'
+
+export default function EmptyState(){
+    const router=useRouter();
+  return (
+    <View style={{
+        marginTop:80,
+        display:'flex',
+        alignItems:'center'
+    }}>
+      <Image source={require('./../assets/images/medicin.png')} 
+      style={{
+        width:120,
+        height:120
+      }}/>
+      <Text style={{
+        fontSize:35,
+        fontWeight:'bold',
+        marginTop:30
+      }}>{ConstantString.NoMedication}</Text>
+
+      <Text style={{
+        fontSize:16,
+        color:colors.DARK_GRAY,
+        textAlign:'center',
+        marginTop:20
+      }}>{ConstantString.MedicationSubText}</Text>
+      <TouchableOpacity style={{
+        backgroundColor:Colors.PRIMARY,
+        padding:15,
+        borderRadius:10,
+        width:'100%',
+        MarginTop:30
+      }} 
+      onPress={()=> router.push('/add-new-medication')}>
+        <Text style={{
+            textAlign:'center',
+            fontSize:17,
+            color:'white'
+        }}>{ConstantString.AddNewMedicationBtn}</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
