43f56b9127a8f50534611645c042ba598fbd931f
Divya-Crypto06 <katta.chowdary13@gmail.com>
2025-02-10 22:55:02 -0500

    Update index.jsx

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, FlatList } from 'react-native'
 import React from 'react'
 import { Redirect } from 'expo-router'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../config/FirebaseConfig'
+import Header from '../../components/Header'
+import EmptyState from '../../config/EmptyState'
+import MedicationList from '../../components/MedicationList'
 
 export default function Homescreen() {
   return (
-    <View>
-      <Text>Homescreen</Text>
-      <Redirect href={'login'} />
-    </View>
+    <FlatList
+    data={[]}
+    ListHeaderComponent={
+      <View style={{
+        padding:25,
+        backgroundColor:'white',
+        height:'100%'
+        
+      }}>
+        <Header/>
+        <MedicationList />
+      </View>
+    }
+    />
+    
   )
 }
